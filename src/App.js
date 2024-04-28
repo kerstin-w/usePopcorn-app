94a7cc36a3ce48974bd791a9113576acdc3b8966
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-28 16:43:39 +0200

    refactor: Create own useMovies hook to fetch movies from omdB

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 } from "./Main";
 import { Loader } from "./Loader";
 import { ErrorMessage } from "./ErrorMessage";
+import { useMovies } from "./useMovies";
 
 /**
  * The `average` function calculates the average value of an array by summing all elements and dividing
@@ -23,14 +24,12 @@ export const KEY = "3b0006a4";
 
 export default function App() {
   const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
+  const [selectedId, setSelectedId] = useState(null);
+  const { movies, isLoading, error } = useMovies(query);
   const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
     return JSON.parse(storedValue);
   });
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [selectedId, setSelectedId] = useState(null);
 
   function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
@@ -53,49 +52,6 @@ export default function App() {
       localStorage.setItem("watched", JSON.stringify(watched));
     },
     [watched]
-  );
-
-  useEffect(
-    function () {
-      const controller = new AbortController();
-
-      async function fetchMovies() {
-        try {
-          setIsLoading(true);
-          setError("");
-          const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
-            { signal: controller.signal }
-          );
-
-          if (!res.ok)
-            throw new Error("Something went wrong with fetching movies");
-
-          const data = await res.json();
-          if (data.Response === "False") throw new Error("Movie not found");
-          setMovies(data.Search);
-          setError("");
-        } catch (err) {
-          if (err.name !== "AbortError") {
-            setError(err.message);
-          }
-        } finally {
-          setIsLoading(false);
-        }
-      }
-      if (query.length < 2) {
-        setMovies([]);
-        setError("");
-        return;
-      }
-      handleCloseMovie();
-      fetchMovies();
-
-      return function () {
-        controller.abort();
-      };
-    },
-    [query]
   );
 
   return (
---
+++ src/useMovies.js
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+
+const KEY = "3b0006a4";
+
+/**
+ * The `useMovies` function fetches movie data based on a query using the OMDB API and manages loading
+ * state and errors.
+ * @param query - The `query` parameter in the `useMovies` function is used to search for movies based
+ * on the user's input. It is the search term that is used to fetch movies from the OMDB API.
+ * @returns The `useMovies` custom hook returns an object with three properties: `movies`, `isLoading`,
+ * and `error`. These properties provide information about the state of the movie search operation. The
+ * `movies` property contains an array of movie objects, `isLoading` indicates whether the search is
+ * currently in progress, and `error` holds any error message encountered during the search process.
+ */
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function fetchMovies() {
+        try {
+          setIsLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok)
+            throw new Error("Something went wrong with fetching movies");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            setError(err.message);
+          }
+        } finally {
+          setIsLoading(false);
+        }
+      }
+      if (query.length < 2) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+
+      fetchMovies();
+
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, isLoading, error };
+}
