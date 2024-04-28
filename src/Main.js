144761a2d0d53df35db0d631427db61f4801f335
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-28 17:22:37 +0200

    fix: Change omdBAPI address to https

--- src/Main.js
+++ src/Main.js
@@ -132,7 +132,7 @@ movie based on the `selectedId` when it changes.*/
       async function getMovieDetails() {
         setIsLoading(true);
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+          `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
         );
         const data = await res.json();
         setMovie(data);
