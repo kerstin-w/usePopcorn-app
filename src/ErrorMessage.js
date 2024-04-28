724c83a5274e5cf45baba9d47c7256f93e8f0d4d
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-28 16:01:49 +0200

    docs: Add comments to ErrorMessage component

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,3 +1,10 @@
+/**
+ * The ErrorMessage function displays an error message with a warning emoji and the
+ * provided message.
+ * @returns The ErrorMessage component is being returned. It is a paragraph element with a span
+ * containing a stop sign emoji (⛔️) followed by the message passed as a prop. The paragraph has a
+ * class name of "error".
+ */
 export function ErrorMessage({ message }) {
   return (
     <p className="error">
