19f8ad01d175fda9fab12177f741a2304e23bd48
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-28 12:25:27 +0200

    feat: useEffect to change page titele depending on the selected movie title

--- src/Main.js
+++ src/Main.js
@@ -103,6 +103,14 @@ export function MovieDetails({
     [selectedId]
   );
 
+  useEffect(
+    function () {
+      if (!title) return;
+      document.title = `Movie | ${title}`;
+    },
+    [title]
+  );
+
   return (
     <div className="details">
       {isLoading ? (
