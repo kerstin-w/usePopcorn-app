75e7e02a8411928715c09c8dbb09da5955518a88
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2024-04-28 12:39:43 +0200

    improvement: Add cleanup function to useEffect for page title. This function will reset the title once the move is onmounted

--- src/Main.js
+++ src/Main.js
@@ -107,6 +107,10 @@ export function MovieDetails({
     function () {
       if (!title) return;
       document.title = `Movie | ${title}`;
+
+      return function () {
+        document.title = "usePopcorn";
+      };
     },
     [title]
   );
